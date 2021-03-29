d554a58fa2deb329b5288cead8ddd897070b6403
Harshit Hajela <hhajela@splunk.com>
2021-03-29 09:55:51 -0600

    DSP-34908: Investigate if disabling slot sharing needs to be retained in 1.12
    Create strategy that maps each execution to its own slot (i.e. no slot
    sharing). Add config option for enabling/disabling it

--- flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/DefaultSchedulerComponents.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/scheduler/DefaultSchedulerComponents.java
@@ -155,12 +155,17 @@ public class DefaultSchedulerComponents {
                         slotPool, SystemClock.getInstance());
         final PhysicalSlotProvider physicalSlotProvider =
                 new PhysicalSlotProviderImpl(slotSelectionStrategy, slotPool);
+        final SlotSharingStrategy.Factory slotSharingStrategyFactory =
+                jobMasterConfiguration.getBoolean(ClusterOptions.DISABLE_SLOT_SHARING)
+                        ? NoSharingSlotSharingStrategy::createInstance
+                        : new LocalInputPreferredSlotSharingStrategy.Factory();
         final ExecutionSlotAllocatorFactory allocatorFactory =
                 new SlotSharingExecutionSlotAllocatorFactory(
                         physicalSlotProvider,
                         scheduleMode != ScheduleMode.LAZY_FROM_SOURCES_WITH_BATCH_SLOT_REQUEST,
                         bulkChecker,
-                        slotRequestTimeout);
+                        slotRequestTimeout,
+                        slotSharingStrategyFactory);
         return new DefaultSchedulerComponents(
                 new PipelinedRegionSchedulingStrategy.Factory(),
                 bulkChecker::start,
